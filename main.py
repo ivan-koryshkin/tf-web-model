ef9624e1d04e3caba9a83c9e499f83aacbba26fd
ivan <ivan.koryshkin@mail.com>
2024-06-08 18:14:31 +0400

    feature(logging): Logging

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
+
+import logging
 import asyncio
 import tornado
 
 from apps import prediction
+
+PORT = int(os.environ["WEB_MODEL_PORT", "8001"])
+
+logger = logging.getLogger("MODEL")
 
 def make_app():
     handlers = [
@@ -11,7 +18,8 @@ def make_app():
 
 async def main():
     app = make_app()
-    app.listen(8000)
+    app.listen(PORT)
+    logger.info(f"Server started on port {PORT}")
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
